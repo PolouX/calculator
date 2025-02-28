d5411891ecec70d333f49d7e2685310e1bdbc84f
PolouX <deoh02@gmail.com>
2025-02-27 23:51:43 -0600

    Added GitHub Actions workflow for tests

--- src/tests/test4.js
+++ src/tests/test4.js
@@ -2,7 +2,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('test', function() {
+describe('Test4', function() {
   this.timeout(30000)
   let driver
   let vars
@@ -13,15 +13,16 @@ describe('test', function() {
   afterEach(async function() {
     await driver.quit();
   })
-  it('test', async function() {
-    await driver.get("http://127.0.0.1:5500/src/index.html")
-    await driver.manage().window().setRect({ width: 720, height: 431 })
+  it('Test4', async function() {
+    await driver.get("http://127.0.0.1:5500/")
+    await driver.manage().window().setRect({ width: 976, height: 528 })
+    await driver.findElement(By.css(".name")).click()
     await driver.findElement(By.id("num1")).click()
-    await driver.findElement(By.id("num1")).sendKeys("9")
+    await driver.findElement(By.id("num1")).sendKeys("0")
     await driver.findElement(By.id("num2")).click()
-    await driver.findElement(By.id("num2")).sendKeys("10")
-    await driver.findElement(By.id("result")).click()
-    await driver.findElement(By.css("button:nth-child(1)")).click()
+    await driver.findElement(By.id("num2")).sendKeys("0")
+    await driver.findElement(By.css("body")).click()
     await driver.findElement(By.css("button:nth-child(2)")).click()
+    await driver.close()
   })
 })
